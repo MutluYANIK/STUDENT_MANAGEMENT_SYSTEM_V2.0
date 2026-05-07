95eee98e2609db3bafb74e939fa37a094d3ae35c
Mutlu YANIK <mutlu_ynk@outlook.com>
2026-05-07 17:33:00 +0300

    Add add_student skeleton and ID validation

---
+++ src/add_student.c
@@ -0,0 +1,25 @@
+#include "../headers/safe_input.h"
+#include "../headers/student_management_system.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int add_student(student **head) {
+
+    
+
+    unsigned int temp = get_safe_unsigned_int(3, "Enter student ID (or 0 to return to main menu): ");
+
+    if (temp == 0) {
+        printf("\nOperation cancelled. Returning to menu...\n\n");
+        return 0;
+    }
+
+    if (id_check(*head, temp) == 1) {
+
+        return 1;
+    }
+
+
+
+    return 1;
+}
