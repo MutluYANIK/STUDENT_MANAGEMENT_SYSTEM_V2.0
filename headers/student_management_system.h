0b8e2ecdec1a741e99183bfe9a0f9d0ab19319e8
Mutlu YANIK <mutlu_ynk@outlook.com>
2026-05-08 23:14:42 +0300

    Feat: add sorting enums and  compare_students prototype to header

--- headers/student_management_system.h
+++ headers/student_management_system.h
@@ -27,10 +27,22 @@ typedef struct student {
     struct student *next;
 } student;
 
+typedef enum current_mode {
+    SORT_BY_ID,
+    SORT_BY_GRADE,
+    SORT_BY_NAME
+} current_mode;
+
+typedef enum current_direction {
+    DIR_NORMAL,
+    DIR_REVERSED
+} current_direction;
+
 int add_student(student **student_head);
 int add_lecture(lecture **lecture_head);
 int id_check(student *student_head, unsigned int id);
 int lecture_id_check(lecture *lecture_head, const char *lecture_id);
 int enrollment_check(student *current_student, lecture *target_lecture);
+int compare_students(student *s1, student *s2);
 
 #endif
