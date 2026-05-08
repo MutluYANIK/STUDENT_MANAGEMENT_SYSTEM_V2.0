e5b91ed6e3a3a86ad83343a0ab9743b50de5e5b3
Mutlu YANIK <mutlu_ynk@outlook.com>
2026-05-08 23:34:04 +0300

    Feat: add get_positive_int prototype to header

--- headers/safe_input.h
+++ headers/safe_input.h
@@ -5,5 +5,6 @@ void get_safe_name(int max_attempts, char *target, int max_length, const char *m
 int get_safe_int_between(int min, int max, int max_attempts, const char *message);
 unsigned int get_safe_unsigned_int(int max_attempts, const char *message);
 void get_safe_string(int max_attempts, char *target, int max_length, const char *message);
+int get_positive_int(int max_attempts, const char *message);
 
 #endif
