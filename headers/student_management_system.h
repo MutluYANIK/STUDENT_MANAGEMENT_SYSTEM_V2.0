9950e2493b56c00b7f9a574bcb00091431fe0965
Mutlu YANIK <mutlu_ynk@outlook.com>
2026-05-08 20:51:40 +0300

    Refactor: update lecture_id parameter to const char to prevent mutation

--- headers/student_management_system.h
+++ headers/student_management_system.h
@@ -30,6 +30,6 @@ typedef struct student {
 int add_student(student **student_head);
 int add_lecture(lecture **lecture_head);
 int id_check(student *student_head, unsigned int id);
-int lecture_id_check(lecture *lecture_head, char *lecture_id);
+int lecture_id_check(lecture *lecture_head, const char *lecture_id);
 
 #endif
