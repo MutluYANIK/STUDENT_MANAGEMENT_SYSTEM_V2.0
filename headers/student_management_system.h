71118f79f86233da6bb53f4dcf7689b1ea7b1e97
Mutlu YANIK <mutlu_ynk@outlook.com>
2026-05-08 23:45:01 +0300

    Feat: add GPA to student struct and credit to lecture struct

--- headers/student_management_system.h
+++ headers/student_management_system.h
@@ -10,11 +10,12 @@ typedef struct exam_template {
 typedef struct lecture {
     char lecture_id[10];
     char lecture_name[40];
+    int lecture_credit;
     struct exam_template *exams;
     struct lecture *next;
 } lecture;
 
-typedef struct enrollment{
+typedef struct enrollment {
     struct lecture *lecture;
     int *scores;
     struct enrollment *next;
@@ -23,20 +24,18 @@ typedef struct enrollment{
 typedef struct student {
     unsigned int id;
     char name[50];
+    float GPA;
     struct enrollment *records;
     struct student *next;
 } student;
 
-typedef enum current_mode {
-    SORT_BY_ID,
-    SORT_BY_GRADE,
-    SORT_BY_NAME
-} current_mode;
+typedef enum { SORT_BY_ID, SORT_BY_GRADE, SORT_BY_NAME } sort_mode;
 
-typedef enum current_direction {
-    DIR_NORMAL,
-    DIR_REVERSED
-} current_direction;
+extern sort_mode current_mode;
+
+typedef enum { DIR_NORMAL, DIR_REVERSED } sort_direction;
+
+extern sort_direction current_direction;
 
 int add_student(student **student_head);
 int add_lecture(lecture **lecture_head);
