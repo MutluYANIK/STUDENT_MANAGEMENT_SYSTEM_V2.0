5925e7b3cc2bbed9d19a182001169a54dad8a520
Mutlu YANIK <mutlu_ynk@outlook.com>
2026-05-09 18:03:34 +0300

    Refactor: remove printf from id_check (moving to add_student)

--- src/id_check.c
+++ src/id_check.c
@@ -6,7 +6,6 @@ int id_check(student *head, unsigned int id) {
 
     while (current != NULL) {
         if (current->id == id) {
-            printf("\n!ERROR! A student with ID %u already exists in the list!\n\n", id);
             return 1;
         }
         current = current->next;
