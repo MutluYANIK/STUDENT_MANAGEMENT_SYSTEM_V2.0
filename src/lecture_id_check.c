1914bbe0b8003c3b7cb0323e8cfbc2fbaafedf09
Mutlu YANIK <mutlu_ynk@outlook.com>
2026-05-09 18:03:59 +0300

    Refactor: remove printf from lecture_id_check (moving to add_lecture)

--- src/lecture_id_check.c
+++ src/lecture_id_check.c
@@ -8,7 +8,6 @@ int lecture_id_check(lecture *lecture_head, const char *lecture_id) {
     
     while (current != NULL) {
         if (strcmp(current->lecture_id, lecture_id) == 0) {
-            printf("\n!ERROR! Lecture ID already exists!");
             return 1;
         }
         current = current->next;
