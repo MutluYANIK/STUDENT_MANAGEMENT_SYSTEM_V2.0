1dd7b845e40e8e324c64366f55e988b51afc19ad
Mutlu YANIK <mutlu_ynk@outlook.com>
2026-05-12 04:04:59 +0300

    Feat: add display_student prototype to header

--- headers/student_management_system.h
+++ headers/student_management_system.h
@@ -61,5 +61,6 @@ void print_enrolled_lectures(student *current_student);
 void print_not_enrolled_lectures(student *current_student, lecture *lecture_head);
 lecture *find_lecture(lecture *lecture_head, const char *target_lecture_id);
 void delete_student(student **student_head, lecture *lecture_head);
+void display_student(student *student_head, lecture *lecture_head);
 
 #endif
